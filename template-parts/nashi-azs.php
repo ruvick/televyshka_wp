71a1adbab56cc7885fceb3480268a4a39fbc1f41
Ruvick <rudikov.web@ya.ru>
2022-11-22 23:49:37 +0300

    Вывел картинки азс, порпавил стили

--- template-parts/nashi-azs.php
+++ template-parts/nashi-azs.php
@@ -2,35 +2,35 @@
 
 <div class="our-gas__inner d-flex"> 
 
-  <a href="<?php echo get_permalink(24);?>" class="our-gas__card our-gas__card_01">
+  <a href="<?php echo get_permalink(24);?>" class="our-gas__card" style="background-image: url(<?php echo wp_get_attachment_image_src(carbon_get_post_meta(get_the_ID(),"zap_img_1"), 'full')[0];?>);">
     <div class="our-gas__card-descpBlock">
       <div class="our-gas__card-descpBlock-icon"></div>
       <div class="our-gas__card-descpBlock-text"><?echo carbon_get_post_meta(get_the_ID(),"zap_addres_1"); ?></div> 
     </div>
   </a>
 
-  <a href="<?php echo get_permalink(26);?>" class="our-gas__card our-gas__card_02">
+  <a href="<?php echo get_permalink(26);?>" class="our-gas__card" style="background-image: url(<?php echo wp_get_attachment_image_src(carbon_get_post_meta(get_the_ID(),"zap_img_2"), 'full')[0];?>);"> 
     <div class="our-gas__card-descpBlock">
       <div class="our-gas__card-descpBlock-icon"></div>
       <div class="our-gas__card-descpBlock-text"><?echo carbon_get_post_meta(get_the_ID(),"zap_addres_2"); ?></div>
     </div>
   </a>
 
-  <a href="<?php echo get_permalink(28);?>" class="our-gas__card our-gas__card_03">
+  <a href="<?php echo get_permalink(28);?>" class="our-gas__card" style="background-image: url(<?php echo wp_get_attachment_image_src(carbon_get_post_meta(get_the_ID(),"zap_img_3"), 'full')[0];?>);">
     <div class="our-gas__card-descpBlock">
       <div class="our-gas__card-descpBlock-icon"></div>
       <div class="our-gas__card-descpBlock-text"><?echo carbon_get_post_meta(get_the_ID(),"zap_addres_3"); ?></div>
     </div>
   </a>
 
-  <a href="<?php echo get_permalink(30);?>" class="our-gas__card our-gas__card_04">
+  <a href="<?php echo get_permalink(30);?>" class="our-gas__card" style="background-image: url(<?php echo wp_get_attachment_image_src(carbon_get_post_meta(get_the_ID(),"zap_img_4"), 'full')[0];?>);">
     <div class="our-gas__card-descpBlock">
       <div class="our-gas__card-descpBlock-icon"></div>
       <div class="our-gas__card-descpBlock-text"><?echo carbon_get_post_meta(get_the_ID(),"zap_addres_4"); ?></div>
     </div>
   </a>
 
-  <a href="<?php echo get_permalink(32);?>" class="our-gas__card our-gas__card_06">
+  <a href="<?php echo get_permalink(32);?>" class="our-gas__card" style="background-image: url(<?php echo wp_get_attachment_image_src(carbon_get_post_meta(get_the_ID(),"zap_img_5"), 'full')[0];?>);">
     <div class="our-gas__card-descpBlock">
       <div class="our-gas__card-descpBlock-icon"></div>
       <div class="our-gas__card-descpBlock-text"><?echo carbon_get_post_meta(get_the_ID(),"zap_addres_5"); ?></div>
@@ -40,7 +40,7 @@
 </div>
 
 <div class="our-gas__inner our-gas__inner-big d-flex">
-  <a href="<?php echo get_permalink(34);?>" class="our-gas__card our-gas__card-big our-gas__card_05">
+  <a href="<?php echo get_permalink(34);?>" class="our-gas__card our-gas__card-big" style="background-image: url(<?php echo wp_get_attachment_image_src(carbon_get_post_meta(get_the_ID(),"zap_img_6"), 'full')[0];?>);">
     <div class="our-gas__card-descpBlock">
       <div class="our-gas__card-descpBlock-icon"></div>
       <div class="our-gas__card-descpBlock-text"><?echo carbon_get_post_meta(get_the_ID(),"zap_addres_6"); ?></div>
