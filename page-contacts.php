1aed86e7e7b2d84313ecbce9272329cc17afd8a8
Ruvick <rudikov.web@ya.ru>
2022-11-23 23:29:50 +0300

    Фавиконы

--- page-contacts.php
+++ page-contacts.php
@@ -27,7 +27,7 @@ get_header(); ?>
 		}
 	?> 
 
-  <ul> 
+  <ul class="contacts-list"> 
 	  <? $org = carbon_get_theme_option("as_company"); if (!empty($org)){?><li>Организация: <strong><? echo $org; ?></strong></li><?}?> 
 		<? $adr = carbon_get_theme_option("as_address"); if (!empty($adr)){?><li>Адрес: <strong><? echo $adr; ?></strong></li><?}?>
 		<? $inn = carbon_get_theme_option("as_inn"); if (!empty($inn)){?><li>ИНН: <strong><? echo $inn; ?></strong></li><?}?>
@@ -38,54 +38,12 @@ get_header(); ?>
 		<? $bik = carbon_get_theme_option("as_bik"); if (!empty($bik)){?><li>БИК: <strong><? echo $bik; ?></strong></li><?}?>
 		<? $bank = carbon_get_theme_option("as_bank"); if (!empty($bank)){?><li>БАНК: <strong><? echo $bank; ?></strong></li><?}?>
 		<? $mail = carbon_get_theme_option("as_email"); if (!empty($mail)){?><li>Email: <strong><a href="mailto:<? echo $mail; ?>"><? echo $mail; ?></strong></a></li><?}?>
-		<? $tel = carbon_get_theme_option("as_phones_1"); $tel2 = carbon_get_theme_option("as_phone_2"); if (!empty($tel)){?><li>Тел: <strong><a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel); ?>"><? echo $tel; ?></strong></a> <a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel2); ?>"><? echo $tel2; ?></strong></a></li><?}?> 
+		<? $tel = carbon_get_theme_option("as_phones_1"); $tel2 = carbon_get_theme_option("as_phone_2"); if (!empty($tel)){?><li class="contacts-list__item-phone">По всем интересующим вопросам обращайтесь по телефону: <strong><a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel); ?>"><? echo $tel; ?></strong></a> <a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel2); ?>"><? echo $tel2; ?></strong></a></li><?}?> 
 	</ul>
 
-		<div class="block__map" id="map"></div>
-		<script src="//api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script> 
+	<?php get_template_part('template-parts/addresses-section');?>
 
-		<script>
-			ymaps.ready(init); 
-
-			function init () {
-				var myMap = new ymaps.Map("map", {
-        // Координаты центра карты
-        center:[<?php echo carbon_get_theme_option('map_point') ?>],
-        // Масштаб карты
-        zoom: 17,
-        // Выключаем все управление картой
-        controls: []
-      }); 
-
-				var myGeoObjects = [];
-
-    // Указываем координаты метки
-    	myGeoObjects = new ymaps.Placemark([<?php echo carbon_get_theme_option('map_point') ?>],{
-    	// hintContent: '<div class="map-hint">Авто профи, Курск, ул.Комарова, 16</div>',
-    	balloonContent: '<div class="map-hint"><?php echo carbon_get_theme_option('text_map') ?>', },{
-    	iconLayout: 'default#image',
-      // Путь до нашей картинки
-      iconImageHref:  '<?php bloginfo("template_url"); ?>/img/icons/map-azs.svg',  
-      // Размеры иконки
-      iconImageSize: [50, 75],
-      // Смещение верхнего угла относительно основания иконки
-      iconImageOffset: [-25, -100]
-    });
-
-    var clusterer = new ymaps.Clusterer({
-    	clusterDisableClickZoom: false,
-    	clusterOpenBalloonOnClick: false,
-    });
-    
-    clusterer.add(myGeoObjects);
-    myMap.geoObjects.add(clusterer);
-    // Отключим zoom
-    myMap.behaviors.disable('scrollZoom');
-
-  }
-</script>
-
-</div>
+	</div>
 </section>
 
 </main>
